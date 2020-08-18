8f6cd780e92db8234270d51b50e56503025b7357
Glitch (crestat) <none>
2020-08-18 22:39:35 +0000

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
 # crestat
-An expedient, super simple, fully tested, zero dependency, generic state management tool. Made specifically for use with crelm.
+
+An expedient, super simple, fully tested, zero dependency, generic state management tool. Made specifically for use with [crelm](https://www.npmjs.com/package/crelm).
+
+### installation
+```cli
+npm i crestat
+```
+### import
+```js
+import crestat from 'crestat'
+const state = crestat()
+// or
+const state = require('crestat')()
+```
+### usage
+```js
+// Set a state value
+state.set(key, value)
+state.set('sample', true)
+
+// Get a state value
+state.get(key)
+let sample = state.get('sample')
+
+// Attach to element/object for auto updates
+let obj = {}
+state.attach(key, elem:Object or querySelector string, attr)
+// If the 2nd argument is a html element reference, the listener will be removed when the element is removed from the DOM
+state.attach('sample', obj, 'innerHTML')
+state.attach('money', '.player-money', 'innerText')
+
+// Attach function
+state.attach(sampleFunction)
+state.attach(key, newValue => {
+  window.sample = newValue
+})
+state.attach(key, newValue => {
+  if (window.isHappy) doTheThings()
+  else return null // returning null will delete the listener
+})
+```
+### import/export
+```js
+  let json = state.toJSON()
+  state.load(json)
+```
+### global state
+```js
+const {global} = require('crestat')
+
+global.attach('test', document.body, 'innerHTML')
+global.set('test', 'Hello World!')
+global.get('test')
+// or
+window.state = require('crestat')()
+```
---
+++ main.spec.js
@@ -0,0 +1,75 @@
+require("browser-env")();
+const crestat = require("./main");
+
+describe("global instance:", () => {
+  let obj = {};
+  let state = crestat.global
+  test("set and get:", () => {
+    state.set("global", true);
+    expect(state.get("global")).toBe(true);
+  });
+  test("attach to object:", () => {
+    state.attach('global', obj, "value");
+    state.set("global", false);
+    expect(obj.value).toBe(false);
+  });
+  test("query selector update:", () => {
+    document.body.innerHTML = '<b></b>'
+    state.attach('global', 'b', "innerHTML");
+    state.set("global", 'false');
+    expect(document.body.firstElementChild.innerHTML).toBe('false');
+  });
+  test("attach function:", () => {
+    let value
+    state.set("global", null)
+    state.attach('global', val => value = val)
+    state.set('global', 'global')
+    expect(value).toBe('global')
+  });
+  test("load:", () => {
+    state.load({'global-load': true})
+    expect(state.get('global-load')).toBe(true)
+  });
+  test("load error:", () => {
+    expect(() => state.load(1)).toThrowError()
+  })
+  test("toJSON:", () => {
+    state.set('global-load', 'toJSON')
+    state.set('global', 'toJSON')
+    let json = state.toJSON()
+    expect(json['global-load']).toBe('toJSON')
+    expect(json['global']).toBe('toJSON')
+  });
+});
+
+describe("local seperation:", () => {
+  let local = crestat();
+  let {global} = crestat
+  let obj = {};
+  test("no access to global values:", () => {
+    expect(local.get("global")).toBe(undefined);
+  });
+  test("set and get:", () => {
+    local.set("local", true);
+    expect(local.get("local")).toBe(true);
+  });
+  test("global no touchy the local:", () => {
+    expect(global.get('local')).toBe(undefined)
+  })
+});
+
+describe('Deletion of subscribers on return null', () => {
+  test('Delete function on return null:', () => {
+    let local = crestat()
+    local.attach('happy', () => {return null})
+    local.set('happy', true)
+    expect(local.data.get('happy').subscribers.length).toBe(0)
+  })
+  test('Delete element on return null:', () => {
+    let local = crestat()
+    let div = document.createElement('div')
+    local.attach('sad', div, 'innerHTML')
+    local.set('sad', true)
+    expect(local.data.get('sad').subscribers.length).toBe(0)
+  })
+})
